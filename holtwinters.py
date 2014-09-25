6f66e1f7131c594e1d47c563c9fe9c916f230942
Jefferson <recife.rodrigo@gmail.com>
2014-09-25 13:39:45 -0300

    Calculados os valores de a0(0), b1(0) e Sn_t(0)

--- holtwinters.py
+++ holtwinters.py
@@ -6,7 +6,7 @@ time_series_soda = [189, 229, 249, 289, 260, 431, 660, 777, 915, 613, 485,
           904, 715, 441]
 
     
-def moving_average(time_series, period_size, period):
+def calculateMovingAverage(time_series, period_size, period):
     current_moving_average = 0.0
     
     for y in time_series[ (period - 1) * period_size : period * period_size ]:
@@ -14,47 +14,51 @@ def moving_average(time_series, period_size, period):
     return current_moving_average/period_size
     
     
-def S(time_series, t, period_size, b1):
+def calculateS(time_series, t, period_size, tendence_component):
     period = (t/period_size) + 1
-    corresponding_moving_average = moving_average(time_series, period_size, period)
+    corresponding_moving_average = calculateMovingAverage(time_series, period_size, period)
     j = t % period_size
     
-    yj_av = corresponding_moving_average - ( ( ( (period_size + 1.0) / 2.0) - (j+1) ) * b1 )
+    yj_av = corresponding_moving_average - ( ( ( (period_size + 1.0) / 2.0) - (j+1) ) * tendence_component )
     return time_series[t] / yj_av
 
+def calculateSazonalComponents(sazonal_averages, L, sazonal_averages_sum):
+    sazonal_components = []
+    for average_sn_t in sazonal_averages:
+        sazonal_components.append( average_sn_t * ( L / sazonal_averages_sum) )
+    return sazonal_components
+
+    
 def hwm(time_series, m, L):
     a0 = []
     b1 = []
     snt = []
-    sn_average = []
-    sn_somat = 0.0
-    St = []
+    sazonal_averages_sum = 0.0
+    S = []
     Sn = []
+    sazonal_averages = []
+    sazonal_components = []
+    sazonal_components_t = []
 
-    first_period_average = moving_average(time_series, L, 1)
-    last_period_average = moving_average(time_series, L, m)
+    first_period_average = calculateMovingAverage(time_series, L, 1)
+    last_period_average = calculateMovingAverage(time_series, L, m)
         
     b1.append( ( last_period_average - first_period_average ) / ( (m - 1) * L ) )
     a0.append( first_period_average - ( (L/2) * b1[0] ) )
     
-    #TODO refatorar daqui para baixo. Os calculos iniciais ja estao ok. 
-    
     for t in range(len(time_series)):
-        St.append(S(time_series, t, L, b1[0]))
+        S.append(calculateS(time_series, t, L, b1[0]))
     
     for t in range(L):
         s_somat = 0.0
         for k in range(m):
-            s_somat = s_somat + St[t + (k * L)]
-        sn_average.append((1.0 / m) * s_somat)
-        sn_somat = sn_somat + ( (1.0 / m) * s_somat )
-    
-    for i in range(L):
-        Sn.append( sn_average[i] * ( L / sn_somat) )
-    print Sn
-    #current_sn_average = 0.0
-    #for i in range(m-1):
-    #    current_sn_average = current_sn_average + S(i)
+            s_somat = s_somat + S[t + (k * L)]
+        sazonal_averages.append((1.0 / m) * s_somat)
+        sazonal_averages_sum = sazonal_averages_sum + ( (1.0 / m) * s_somat )
+
+    sazonal_components.append( calculateSazonalComponents(sazonal_averages, L, sazonal_averages_sum) )
+    #Agora ja temos a0(0), b1(0) e Sn_t(0)
+
     
 
 def main():
