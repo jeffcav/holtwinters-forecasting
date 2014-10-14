fefc90ec33de2a4d9e8580fe971580ac0c04940a
Jefferson Rodrigo <recife.rodrigo@gmail.com>
2014-10-13 23:50:49 -0300

    Fix bugs. Calculate component and estimate.
    The calculate components is now working well.
    Estimation is ok.

--- holtwinters.py
+++ holtwinters.py
@@ -8,7 +8,7 @@ class HoltWintersEstimator(Estimator):
         seasonal_factor: Indicates the seasonal factor of each observation on your data set.
         average_component: Indicates the average component in the Holt-Winters model.
         tendence_component: Indicates the tendence component in the Holt-Winters model.
-        seasonal_component: Indicates the seasonal component in the Holt-Winters model.
+        seasonal_component: Indicates the seasonal component in the Holt-Winters model. It is a matrix whose first index is the seasonal index of a given time, and the second index is the season itself. 
     """
 
     def __init__(self, time_series, number_of_seasons, season_size):
@@ -21,7 +21,7 @@ class HoltWintersEstimator(Estimator):
         self.tendence_component = []
         self.seasonal_component = []
         self.initializeComponents()
-        #self.calculateComponents(0.2, 0.15, 0.05)
+        self.calculateComponents(0.2, 0.15, 0.05)
 
     def seasonOfTime(self, time):
         """ Returns the season which holds the data at a given time, from 1 to number_of_seasons """
@@ -62,6 +62,8 @@ class HoltWintersEstimator(Estimator):
         return factor
 
     def insertSeasonalComponent(self, seasonal_index, season, value):
+        """ Inserts a value in the seasonal component matrix.
+        """
         if (len(self.seasonal_component)) < seasonal_index:
             while len(self.seasonal_component) < seasonal_index:
                 self.seasonal_component.append([])
@@ -69,10 +71,11 @@ class HoltWintersEstimator(Estimator):
         if len(self.seasonal_component[seasonal_index - 1]) < (season + 1):
             while len(self.seasonal_component[seasonal_index - 1]) < (season + 1):
                 self.seasonal_component[seasonal_index - 1].append(None)
-        
         self.seasonal_component[seasonal_index - 1][season] = value
 
     def getSeasonalComponent(self, time):
+        """ Returns a value from the seasonal component matrix.
+        """
         if time <= 0:
             season = 0
             seasonal_index = self.seasonalIndexOfTime(time + self.season_size)
@@ -83,7 +86,7 @@ class HoltWintersEstimator(Estimator):
 
     def estimate(self, time, base_time):
         """ Estimate the value of the function on time, based on the base_time observation. Often base_time is time-1. """
-        estimation = (self.average_component[base_time] + self.tendence_component[base_time] * time) * self.getSeasonalComponent(time - self.season_size)
+        estimation = (self.average_component[base_time] + self.tendence_component[base_time] * (time - base_time)) * self.getSeasonalComponent(time - self.season_size)
         return estimation
 
     def initializeComponents(self):
@@ -109,13 +112,11 @@ class HoltWintersEstimator(Estimator):
                 index = seasonal_index + (m * self.season_size)
                 factor = self.seasonal_factor[index - 1]
                 seasonal_index_sum += factor
-
             seasonal_index_average.append(seasonal_index_sum *  (1.0 / self.number_of_seasons))
 
         snt_average_sum = 0.0
         for snt_average in seasonal_index_average:
             snt_average_sum += snt_average
-        
         adjustment_level = self.season_size / snt_average_sum
 
         for seasonal_index in range(1, self.season_size + 1):
@@ -124,7 +125,6 @@ class HoltWintersEstimator(Estimator):
 
     def calculateComponents(self, alpha, beta, gamma):
         for time in range(1, len(self.time_series) + 1):
-
             average_component = (alpha * (self.time_series[time - 1] / self.getSeasonalComponent(time - self.season_size))) + ((1 - alpha) * (self.average_component[time - 1] + self.tendence_component[time - 1]))
             self.average_component.append(average_component)
 
@@ -134,7 +134,7 @@ class HoltWintersEstimator(Estimator):
             seasonal_component = gamma * self.time_series[time - 1] / self.average_component[time] + (1 - gamma) * self.getSeasonalComponent(time - self.season_size)
             index = self.seasonalIndexOfTime(time)
             season = self.seasonOfTime(time)
-            self.insertSeasonalComponent(time, season, seasonal_component)
+            self.insertSeasonalComponent(index, season, seasonal_component)
 
 time_series_soda = [189, 229, 249, 289, 260, 431, 660, 777, 915, 613, 485, 277, 244, 296, 319, 370, 313, 556, 831, 960, 1152, 759, 607, 371, 298, 378, 373, 443, 374, 660, 1004, 1153, 1388, 904, 715, 441]
 hwe = HoltWintersEstimator(time_series_soda, 3, 12)
